ecf630c963a33fd544e808e08f7124c1bcc29e66
Aileen Liu <aileenliu184@gmail.com>
2019-07-25 22:23:06 -0700

    errors fixed

--- coding-practice/server.py
+++ coding-practice/server.py
@@ -12,15 +12,38 @@ def show_homepage():
 # 1) Finish the routes below. #
 #                             #
 ###############################
+@app.route('/save-name')
+def save_name():
+
+    name = request.args.get("fullname")
+
+    session['name'] = name
+
+    return render_template('homepage.html')
 
 
 @app.route('/form')
 def show_form():
-    pass
+    return render_template('form.html')
+
 
 @app.route('/results')
 def show_results():
-    pass
+
+    cheery = request.args.get('cheery')
+    honest = request.args.get('honest')
+    dreary = request.args.get('dreary')
+
+    # app.logger.info("message: %s", message)
+
+    if cheery:
+        message = "hello!"
+    elif honest:
+        message = "world!"
+    elif dreary:
+        message = "yahhh"
+
+    return render_template('results.html', message=message)
 
 
 if __name__ == "__main__":
